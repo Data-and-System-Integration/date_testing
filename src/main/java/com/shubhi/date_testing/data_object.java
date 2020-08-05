08c2354f6037f4d6d95498c22f9a1ae2b027fcea
sjain <>
2020-08-05 08:49:45 -0400

    {/src/main/java/com/shubhi/date_testing/data_object.java}

--- src/main/java/com/shubhi/date_testing/data_object.java
+++ src/main/java/com/shubhi/date_testing/data_object.java
@@ -1,4 +1,6 @@
 package com.shubhi.date_testing;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 /**
@@ -16,19 +18,43 @@ public class data_object implements java.io.Serializable {
 	}
 
 	public java.util.Date getStart_date() {
-		return this.start_date;
+	    SimpleDateFormat DateFor = new SimpleDateFormat("dd-MM-yyyy");
+	    Date date = new Date();
+	    try{
+        date = DateFor.parse(this.start_date.toString());
+        }catch (ParseException e) {e.printStackTrace();}
+        
+		return date;
 	}
 
 	public void setStart_date(java.util.Date start_date) {
-		this.start_date = start_date;
+		SimpleDateFormat DateFor = new SimpleDateFormat("dd-MM-yyyy");
+	    Date date = new Date();
+	    try{
+        date = DateFor.parse(start_date.toString());
+        }catch (ParseException e) {e.printStackTrace();}
+        
+		this.start_date = date;
 	}
 
 	public java.util.Date getEnd_date() {
-		return this.end_date;
+		SimpleDateFormat DateFor = new SimpleDateFormat("dd-MM-yyyy");
+	    Date date = new Date();
+	    try{
+        date = DateFor.parse(this.end_date.toString());
+        }catch (ParseException e) {e.printStackTrace();}
+        
+		return date;
 	}
 
 	public void setEnd_date(java.util.Date end_date) {
-		this.end_date = end_date;
+	    SimpleDateFormat DateFor = new SimpleDateFormat("dd-MM-yyyy");
+	    Date date = new Date();
+	    try{
+        date = DateFor.parse(end_date.toString());
+        }catch (ParseException e) {e.printStackTrace();}
+        
+		this.end_date = date;
 	}
 
 	public data_object(java.util.Date start_date, java.util.Date end_date) {
